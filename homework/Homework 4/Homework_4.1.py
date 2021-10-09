9dbd207cd3802d0f7465a0045cc09a6050f24b2b
Andre <Andre@pryanosti.su>
2021-10-09 19:05:10 +0300

    Hw 4 task 1 - done

---
+++ homework/Homework 4/Homework_4.1.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+if len(argv) < 4:
+   print(f'input data (productivity, salary per hour, Bonuses)!')
+else:
+    a = float(argv[1])
+    b = float(argv[2])
+    c = float(argv[3])
+    result = a * b + c
+    print(argv)
+    print(result)
